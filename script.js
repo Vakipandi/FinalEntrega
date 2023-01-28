c5a61eb585ea9e6f7f2c3040abef4ac1b960629e
mau <mauriciomamani306@gmail.com>
2023-01-28 07:26:04 -0500

    correccion

--- script.js
+++ script.js
@@ -4,32 +4,32 @@ function calculaPromedio(user,not1,not2,not3){
     let promedio = parseInt(suma / 3);
 
     if(promedio >= 15 && promedio <= 20){
-        alert `El estudiante ${user} ha aprobado la materia.`
+        alert (`El estudiante ${user}, ha aprobado la materia.`)
     }
     else if(promedio > 10 && promedio < 15){
-        alert `El estudiante ${user} ha aprobado la materia, pero debera esforzarse más.`
+        alert (`El estudiante ${user}, ha aprobado la materia, pero debera esforzarse más.`)
     }else{
-        alert `El estudiante ${user} reprobó la materia`
+        alert (`El estudiante ${user}, reprobó la materia`)
     }
 }
 
 function validaDato (dato){
     while (isNaN(dato)){
-        alert("Ingrese un valor numérico por favor")
-        dato = parseInt(prompt("Decime tu edad"))
+        alert("Ingrese un valor numérico, por favor corrija")
+        dato = parseInt(prompt("Vuelve a escribir la nota"))
     }
     return dato
 }
 
-let cantParticipantes = parseInt(prompt("Ingrese la cantidad de participantes"))
+let cantParticipantes = parseInt(prompt("Ingrese la cantidad de alumnos"))
 
 for ( let i = 0; i < cantParticipantes; i++){
     let person=prompt("Ingrese su nombre:")
-    let nota1 = parseInt(prompt(`Ingrese su primera nota ${person}`))
+    let nota1 = parseInt(prompt(`Ingrese la primera nota de ${person}:`))
     nota1 = validaDato(nota1)
-    let nota2 = parseInt(prompt(`Ingrese su segunda nota ${person}`))
+    let nota2 = parseInt(prompt(`Ingrese la segunda nota de ${person}:`))
     nota2 = validaDato(nota2)
-    let nota3 = parseInt(prompt(`Ingrese su tercera nota ${person}`))
+    let nota3 = parseInt(prompt(`Ingrese la tercera nota de ${person}:`))
     nota3 = validaDato(nota3)
 
     calculaPromedio(person,nota1,nota2,nota3)
