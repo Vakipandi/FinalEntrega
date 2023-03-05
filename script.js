ad52db62afafd873c1232b9e89f1163e2a162989
mau <mauriciomamani306@gmail.com>
2023-03-05 10:39:02 -0500

    carrito

--- script.js
+++ script.js
@@ -1,71 +1,28 @@
-class Producto{
-    constructor(id,nombre,precio,stock,imagen){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.imagen = imagen;
-    }
-    restaStock(){
-        this.stock = this.stock - 1;
-        console.log(`El stock de ${this.nombre} ha sido actualizado`);
-    }
+let carrito = [];
+let total = 0;
+
+
+function agregarProducto(nombre,precio){
+
+    const producto = {
+        nombre: nombre,
+        precio: precio
+    };
+
+    carrito.push(producto);
+
+    const listaCarro = document.getElementById("lista-carro");
+    const elemento = document.createElement("li");
+    elemento.innerText = `${nombre} -- s/. ${precio} soles`;
+    listaCarro.appendChild(elemento);
+
+    total += precio;
+    const totalCarrito = document.getElementById("total-carro");
+    totalCarrito.innerText = `EL total a pagar seria S/. `+ total.toFixed(2) + " soles";
+    
 }
 
-// Creamos los productos
-
-const producto0 = new Producto(00,"Miel",15, 50, "miel.jpg");
-const producto1 = new Producto(01,"Crema de Maní",20, 75, "cremademani.jpg");
-const producto2 = new Producto(02,"Mermelada",8, 37, "mermelada.jpg");
-const producto3 = new Producto(03,"Stevia",18, 46, "stevia.jpg");
-
-const productos = [producto0,producto1,producto2];
-
-productos.push(producto3);
-
-const cardBootstrap = (listaStock) => {
-    for(e of listaStock){
-        let card = document.createElement("div");
-        card.innerHTML = `<div class="card" style="width: 18rem;">
-        <img src="./img/${e.imagen}" class="card-img-top" alt="Aca va la imagen de ${e.nombre}">
-        <div class="card-body">
-          <h5 class="card-title">${e.nombre}</h5>
-          <p class="card-text">Llevalo a tan solo S/.${e.precio} soles</p>
-          <input type="button" onclick="agregaCarrito(${e.id})"  class="btn btn-primary" value="Agregar carrito."></input>
-        </div>
-      </div>`
-      document.body.append(card)
-    }
+function reiniciar(){
+    alert("Compra realizada.");
+    location.reload();
 }
-
-cardBootstrap(productos);
-
-const carritoCompra = [];
-
-class ObjCarrito{
-    constructor(producto,cantidad){
-        this.producto = producto;
-        this.cantidad = cantidad;
-    }
-
-    sumaStock(){
-        this.cantidad = this.cantidad + 1;
-    }
-}
-
-function agregaCarrito(prod){
-    let existeCarrito = carritoCompra.find(e => e.producto == prod)
-
-    if (existeCarrito != undefined){
-        let position = carritoCompra.findIndex(elem => elem.producto == existeCarrito.producto);
-        carritoCompra[position].sumaStock();
-        console.table(carritoCompra);
-
-    }else{
-        const alCarrito = new ObjCarrito(prod,1);
-        carritoCompra.push(alCarrito);
-        console.table(carritoCompra);
-    }
-}
-
-
